06af9def0bb4dfcd3d2bc8b7134d7796ba37344e
Boston Lowrey <bostonlowrey@gmail.com>
2023-06-04 22:22:15 -0400

    test: add tests for user show page and playlist create page

---
+++ spec/features/users/show_spec.rb
@@ -0,0 +1,32 @@
+require 'rails helper'
+
+RSpec.describe 'User show Page', type: :feature do
+  before(:each) do
+    @user = User.create!()
+    visit '/dashboard'
+  end
+  describe "when I visit a user's show page" do
+    it 'I see a header that has the users name' do
+      expect(page).to have_content("#{user.name} Juke Page")
+    end
+    
+    it 'I see a link to create a Juke, join a Juke, and explore nearby Jukes' do
+      expect(page).to have_link("New Juke!")
+      expect(page).to have_link("Join a Juke!")
+      expect(page).to have_link("Explore Nearby Jukes!")
+    end
+
+    it 'I see a list of current jukes, ones I am hosting and ones I am invited to' do
+      expect(page).to have_content("Current Jukes:")
+      within '#hosted' do
+        expect(page).to have_content("Hosted:")
+        expect(page).to have_link()
+      end
+
+      within '#invited' do
+        expect(page).to have_content("Invited:")
+        expect(page).to have_link()
+      end
+    end
+  end
+end
