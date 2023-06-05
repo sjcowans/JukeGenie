b3ee013123d2556be750a38ba539604ab0916029
Boston Lowrey <bostonlowrey@gmail.com>
2023-06-05 14:40:05 -0400

    bug fix

--- spec/features/user_playlists/new_spec.rb
+++ spec/features/user_playlists/new_spec.rb
@@ -14,21 +14,21 @@ RSpec.describe 'New Playlist Page', type: :feature do
     end
 
     xit 'I can fill in the form and submit it' do
-       fill_in "Juke Name", with : "Bob's Playlist"
-       check "Y"
-       fill_in "Address", with : "example address"
-       fill_in "Range", with : ""
-       click_button "Get Started!"
-       expect(current_path).to eq()
+      #  fill_in "Juke Name", with : "Bob's Playlist"
+      #  check "Y"
+      #  fill_in "Address", with : "example address"
+      #  fill_in "Range", with : ""
+      #  click_button "Get Started!"
+      #  expect(current_path).to eq()
     end
 
     xit 'sad path form fillout' do
-       fill_in "Juke Name", with : ""
-       check "Y"
-       fill_in "Address", with : "example address"
-       fill_in "Range", with : ""
-       click_button "Get Started!"
-       expect(page).to have_content("Please fill out all feilds.")
+      #  fill_in "Juke Name", with : ""
+      #  check "Y"
+      #  fill_in "Address", with : "example address"
+      #  fill_in "Range", with : ""
+      #  click_button "Get Started!"
+      #  expect(page).to have_content("Please fill out all feilds.")
     end
   end
 end
