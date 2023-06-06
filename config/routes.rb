bb17c1d2e5f000509e89caac4c8e24177606019a
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-06-06 17:44:28 -0600

    Install Geocoder gem, create view&controller for address determination, create controller&view for Jukeindex call from backend

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get '/dashboard', to: 'users#show'
   get '/users/:id/requests', to: 'requests#show'
   get '/users/:id/playlists', to: 'playlists#show'
+  get '/users/:id/explorejukes', to: 'explorejukes#show'
+  get '/users/:id/jukes', to: 'jukes#index'
   patch '/dashboard', to: 'users#update'
   resources :confirmations, only: %i[create edit], param: :confirmation_token
 end
