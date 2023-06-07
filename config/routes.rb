3fb0096c3a1ead05049fd11a15962ba37c089303
Sean Cowans <76144522+sjcowans@users.noreply.github.com>
2023-06-07 12:36:02 -0600

    feat: Add playlist suggestions page and edit styling

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get '/users/:id/requests', to: 'requests#show'
   get '/users/:id/playlists', to: 'playlists#show'
   patch '/dashboard', to: 'users#update'
+  get "/users/:id/playlists/suggestions", to: 'playlist_suggestions#index'
   resources :confirmations, only: %i[create edit], param: :confirmation_token
 end
