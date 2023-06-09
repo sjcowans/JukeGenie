5a66fafc95432742364ec6034687d91efeddff53
Sean Cowans <76144522+sjcowans@users.noreply.github.com>
2023-06-08 18:13:16 -0600

    heroku4....

--- app/services/genie_service.rb
+++ app/services/genie_service.rb
@@ -9,7 +9,7 @@ class GenieService
   end
 
   def conn
-    Faraday.new(url: 'http://https://juke-genie-api.herokuapp.com')
+    Faraday.new(url: 'https://juke-genie-api.herokuapp.com')
   end
 
   def user
